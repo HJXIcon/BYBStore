fd8926a5cccee26624181701066834cf11e30617
晓梦影 <wangjinxin@icon-XdeMBP.lan>
2017-10-12 10:44:10 +0800

    更新

--- BYBStore/BYBStore/Classes/Home（首页）/SellingList(热销榜)/Controller/BYBSellingListViewController.m
+++ BYBStore/BYBStore/Classes/Home（首页）/SellingList(热销榜)/Controller/BYBSellingListViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "BYBSellingListViewController.h"
+#import "JXPageView.h"
 
 @interface BYBSellingListViewController ()
 
@@ -17,6 +18,32 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.navigationItem.title = @"热销榜";
+    
+    NSArray *titles = @[@"专题",@"服饰",@"美妆",@"母婴",@"轻奢",@"百货",@"美食",@"运动",@"日淘",@"美淘",@"             "];
+    JXPageStyle *style = [[JXPageStyle alloc]init];
+    style.isNeedScale = NO;
+    style.isScrollEnable = YES;
+    style.isShowBottomLine = YES;
+    style.bottomLineColor = BYBThemeColor;
+    style.normalColor = [UIColor blackColor];
+    style.selectColor = BYBThemeColor;
+    style.titleFont = [UIFont boldSystemFontOfSize:17];
+    style.titleHeight = 50;
+    
+    NSMutableArray *childVcs = [NSMutableArray array];
+    
+    for (int i = 0; i < titles.count; i++){
+        UIViewController *vc = [[UIViewController alloc]init];
+        vc.view.backgroundColor = [UIColor colorWithRed:((float)arc4random_uniform(256) / 255.0) green:((float)arc4random_uniform(256) / 255.0) blue:((float)arc4random_uniform(256) / 255.0) alpha:1.0];
+        [childVcs addObject:vc];
+    }
+    
+    CGRect pageViewFrame = CGRectMake(0, 0, self.view.bounds.size.width, self.view.bounds.size.height - 64);
+    
+    JXPageView *pageView = [[JXPageView alloc]initWithFrame:pageViewFrame titles:titles style:style childVcs:childVcs parentVc:self];
+
+    [self.view addSubview:pageView];
+    
 }
 
 - (void)dealloc{
